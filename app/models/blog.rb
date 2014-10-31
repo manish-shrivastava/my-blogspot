249ad311b71a17e76691da162c1d6fee36922669
ror-infobeans <manish.shrivastava@infobeans.com>
2014-10-31 16:50:18 +0530

    Committing full my blog application

---
+++ app/models/blog.rb
@@ -0,0 +1,15 @@
+class Blog < ActiveRecord::Base
+
+  #validations in blog
+   validates_presence_of :title, :body
+   validates_uniqueness_of :title
+   belongs_to :user
+
+  #association with comments
+   has_many :comments, :dependent => :destroy
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
+
+end
